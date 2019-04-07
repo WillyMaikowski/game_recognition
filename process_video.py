141908775580b78a1f1face20f0c5fcdbd813a15
WillyMaikowski <willy.maikowski@gmail.com>
2019-04-07 02:06:18 +0000

    adding processor

---
+++ process_video.py
@@ -0,0 +1,49 @@
+import numpy as np
+import math
+import cv2
+
+cap = cv2.VideoCapture( 'crlvideo.mp4' )
+fps = math.ceil( cap.get( cv2.CAP_PROP_FPS ) )
+
+fourcc = cv2.VideoWriter_fourcc(*'FMP4')
+framesize = ( cap.get( cv2.CAP_PROP_FRAME_WIDTH ), cap.get( cv2.CAP_PROP_FRAME_HEIGHT ) )
+
+line = []
+base = cv2.imread( 'base.png' )
+for h in base: line.append( h[10] )
+line = np.array( line )
+
+
+c = 0
+ini = 0
+games = []
+started = False
+while( cap.isOpened() ):
+	ret, frame = cap.read()
+	if not ret: continue
+	c += 1
+
+	line2 = []
+	for h in frame: line2.append( h[10] )
+	line2 = np.array( line2 )
+
+	diff = np.mean( cv2.absdiff( line, line2 ) )
+	if diff > 10:
+		if started:
+			started = False
+			#out.release()
+			games.append( ( ini, c ) )
+
+		continue
+
+	if not started:
+		started = True
+		#out = cv2.VideoWriter( 'frame_'+str(c)+'.mp4', fourcc, fps, framesize )
+		ini = c
+
+	#out.write( frame )
+
+
+cap.release()
+cv2.destroyAllWindows()
+print( games )
